b961f58719c70295c92e15950455f1b692fefa60
arina <arinapereteatcu0@gmail.com>
2023-10-27 19:14:35 +0300

    added code to status class

--- Lab2/Status.java
+++ Lab2/Status.java
@@ -1,2 +1,29 @@
-public class Status {
+import java.io.File;
+import java.util.Date;
+
+public class Status extends File {
+    private Date lastSnapshotTime;
+
+    public Status(Date lastSnapshotTime) {
+        this.lastSnapshotTime = lastSnapshotTime;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        String folderPath = "C:\\Users\\arina\\IdeaProjects\\OOPLABS\\Lab2\\files";
+        File folder = new File(folderPath);
+
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                System.out.println("Created Snapshot at: " + lastSnapshotTime);
+
+                for (File file : files) {
+                    Date lastModifiedTime = new Date(file.lastModified());
+                    String status = (lastModifiedTime.after(lastSnapshotTime)) ? "Changed" : "No Change";
+                    System.out.println(file.getName() + " - " + status);
+                }
+            }
+        }
+    }
 }
