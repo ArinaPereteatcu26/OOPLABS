032b759199da80d23fb218be223aa940d80a519c
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2023-11-22 15:48:54 +0200

    fix main

--- Lab3/Main.java
+++ Lab3/Main.java
@@ -9,11 +9,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Which DataStructure do you need? :");
+            System.out.println("Which data structure do you need?:");
             System.out.println("1. ArrayUpStack");
             System.out.println("2. LinkedStack");
-            System.out.println("3. ArrayUpQueue");
-            System.out.println("4. LinkedQueue");
+            System.out.println("3.ArrayDownStack");
+            System.out.println("4. ArrayUpQueue");
+            System.out.println("5. LinkedQueue");
+            System.out.println("6. ArrayDownQueue");
             System.out.println("0. Exit");
 
             int dataStructureChoice = scanner.nextInt();
@@ -30,14 +32,24 @@ public class Main {
                     linkedStackOperations.performStackBehaviour(scanner);
                     break;
                 case 3:
+                    Stack arrayDownStack = new ArrayDownStack();
+                    StackBehaviour arrayDownStackOperations = new StackBehaviour(arrayDownStack);
+                    arrayDownStackOperations.performStackBehaviour(scanner);
+                    break;
+                case 4:
                     Queue arrayUpQueue = new ArrayUpQueue();
                     QueueBehaviour arrayUpQueueOperations = new QueueBehaviour(arrayUpQueue);
                     arrayUpQueueOperations.performQueueBehaviour(scanner);
                     break;
-                case 4:
+                case 5:
                     Queue linkedQueue = new LinkedQueue();
                     QueueBehaviour linkedQueueOperations = new QueueBehaviour(linkedQueue);
                     linkedQueueOperations.performQueueBehaviour(scanner);
+                    break;
+                case 6:
+                    Queue arrayDownQueue = new ArrayDownQueue();
+                    QueueBehaviour arrayDownQueueOperations = new QueueBehaviour(arrayDownQueue);
+                    arrayDownQueueOperations.performQueueBehaviour(scanner);
                     break;
                 case 0:
                     System.out.println("q");
